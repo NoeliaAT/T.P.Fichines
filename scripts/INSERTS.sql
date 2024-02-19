413a0b92a084bf0d138d0acebd2901eff6762d22
sara <sara1330castro@gmail.com>
2024-02-19 15:04:13 -0300

    Source

--- scripts/INSERTS.sql
+++ scripts/INSERTS.sql
@@ -16,7 +16,7 @@ START TRANSACTION;
 	CALL altaFichin(001, "Suggar", 2000, 30.50);
 	CALL altaTarjeta(321, 5.5);
 	CALL altaRecarga(@idRecarga, @DniNoelia, 321, '2023-11-21 16:32:04', 49.70);
-	CALL altaJuegaFichin(@idJuegaFichin, @DniNoelia, 321, 001, '2023-11-21 16:40:08', 3000.50);
-	CALL registrarCliente(@DniNoelia, "Noelia", "Almaraz", "Noe@gmail", 54321);
+	CALL altaJuegaFichin(@idJuegaFichin, @DniNoelia, 321, 001, '2023-11-21 16:40:08', 3000.50); --tiene error--
+	CALL registrarCliente(@DniNoelia, "Noelia", "Almaraz", "Noe@gmail", 54321); --tiene error--
 
 COMMIT;
