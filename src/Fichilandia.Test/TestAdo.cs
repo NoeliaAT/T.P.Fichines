0629239a7bdf262c88779f01e2b7bd8e30463006
Ramon-Ariel-ET12 <ramonaielet12d1@gmail.com>
2024-03-01 01:56:25 -0300

    .

--- src/Fichilandia.Test/TestAdo.cs
+++ src/Fichilandia.Test/TestAdo.cs
@@ -8,7 +8,7 @@ namespace Fichilandia.Test;
 public class TestAdo
 {
     protected readonly IAdo Ado;
-    private const string _cadena = "Server=localhost;Database=5to_Fichines;Uid=5to_agbd;pwd=Trigg3rs!;Allow User Variables=True";
+    private const string _cadena = "Server=localhost;Database=5to_Fichines;Uid=root;pwd=root;Allow User Variables=True";
     public TestAdo() => Ado = new AdoDapper(_cadena);
     public TestAdo(string cadena) => Ado = new AdoDapper(cadena);
 }
